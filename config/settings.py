482d3f8b80f647636aa22ea1c5465945b8fa2606
jeonghens <jhyeonseo0523@gmail.com>
2023-11-26 13:00:29 +0900

    [Chore] App 생성: users, devices, classrooms, rental_records

--- config/settings.py
+++ config/settings.py
@@ -55,7 +55,10 @@ DJANGO_APPS = [
 ]
 
 PROJECT_APPS = [
-
+    'users',
+    'devices',
+    'classrooms',
+    'rental_records',
 ]
 
 THIRD_PARTY_APPS = [
@@ -68,7 +71,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
